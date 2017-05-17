83272cc2320b7910c0cbea55d7d8095a86a048c1
Tarciso Braz <tarcisocomp@gmail.com>
2017-05-17 12:14:24 -0300

    Adding single_trip closest trip type, to return the recommendation for a single trip input.

--- best_trip_recommender/best_trip_recommender_api.R
+++ best_trip_recommender/best_trip_recommender_api.R
@@ -4,37 +4,44 @@ source(paste0(api.folder.path, "/feature_factory.R"))
 source(paste0(api.folder.path, "/IO_service.R"))
 
 #* @get /get_best_trips
-get_best_trips <- function(route, time, date, bus_stop_id, closest_trip_type = "next_hour"){
+get_best_trips <- function(route, time, date, bus_stop_id, closest_trip_type="next_hour"){
   get.prediction.data(file = training.data.filepath, time.window = months(3))
   get.trips.schedule()
   check.last.passengers.model.update()
-  
+
   switch(
    closest_trip_type,
    "next_hour" = {
      n.closest.trips <- getNextHourTrips(trips.schedule, route, time, date, bus_stop_id)
-   }, 
-   {
+   },
+   "single_trip" = {
+     n.closest.trips <- getNextHourTrips(trips.schedule, route, time, date, bus_stop_id) 
+   },
+   "surrounding_trips" = {
      n.closest.trips <- getProbableClosestTrips(trips.schedule, route, time, date, bus_stop_id)
    }
   )
-  
+
   if(nrow(n.closest.trips) == 0) {
     return("Rota não encontrada neste dia da semana ou nesta parada")
   }
-  
+
+  print("Extracting features")
   n.closest.trips <- user.feature.extractor(n.closest.trips, date)
 
+  print("Predicting no. passengers")
   n.closest.trips <- get.prediction.passengers.number(n.closest.trips, training.method = prediction.method)
 
+  print("Predicting trip duration")
   n.closest.trips <- get.prediction.trip.duration(n.closest.trips, training.method = prediction.method)
-  
+
+  if (closest_trip_type == "single_trip") return(filter(n.closest.trips,row_number() == 1))
+
   n.closest.trips <- n.closest.trips %>%
     mutate(
       is.fastest.trip = (trip.duration == min(trip.duration)),
       is.emptiest.trip = (passengers.number == min(passengers.number))
     )
-
   return(n.closest.trips)
 }
 
@@ -44,10 +51,10 @@ train_model <<- function() {
 
   trip.duration.model.temp <- train.trip.duration(prediction.data, training.method = prediction.method)
   passengers.number.model.temp <- train.passengers.number(prediction.data, training.method = prediction.method)
-  
+
   trip.duration.model <<- trip.duration.model.temp
   passengers.number.model <<- passengers.number.model.temp
-  
+
   save.models()
   get.last.time.updated.models(force = TRUE)
   return(TRUE)
@@ -63,6 +70,6 @@ print("Loading modules...")
 loaded.models <- load.models()
 if (!loaded.models) {
     print("Modules could not be loaded. Training models.")
-    training.time <- system.time({ train_model() }) 
+    training.time <- system.time({ train_model() })
     print(paste("Training Time:",training.time[3]))
 }
